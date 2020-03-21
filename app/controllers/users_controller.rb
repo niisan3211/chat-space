48edd43292517c279219fcb822ec621e619266a6
niisan3211 <kappahaookikunaritai@icloud.com>
2020-03-21 16:27:58 +0900

    add:update-action
    updateアクションを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def edit
+    
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+      
+        # 中身は↓と同じ
+        # @user = User.find(params[:id])
+        # @user.update(user_params)
+        # redirect_to root_path
+    else
+      render :edit
+    end
+    
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:nickname, :email)
+    end
+
+end
