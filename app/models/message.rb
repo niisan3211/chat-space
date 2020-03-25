f3236b54940dc08a7fcd8ccdb8abacef3a49df30
niisan3211 <kappahaookikunaritai@icloud.com>
2020-03-26 03:08:04 +0900

    fix:validation
    バリデーションはエラーが出た為、カリキュラム通りに記述する

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,10 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  with_options if: :published? do
-    validates :body, presence: true
-    validates :image, presence: true
-  end
-
+  validates :body, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
 end
